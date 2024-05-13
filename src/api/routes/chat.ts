35a7563b8500a1a49ba978f62c69f0983e2aee8f
lbls888 <131499083+lbls888@users.noreply.github.com>
2024-05-13 17:01:40 +0800

    Update chat.ts

--- src/api/routes/chat.ts
+++ src/api/routes/chat.ts
@@ -14,13 +14,13 @@ export default {
 
         '/completions': async (request: Request) => {
             request
-                。validate('body.model', v => _.isUndefined(v) || _.isString(v))
-                。validate('body.messages', _.isArray)
-                。validate('body.tempature', v => _.isUndefined(v) || _.isNumber(v))
-                。validate('headers.authorization', _.isString)
+                .validate('body.model', v => _.isUndefined(v) || _.isString(v))
+                .validate('body.messages', _.isArray)
+                .validate('body.tempature', v => _.isUndefined(v) || _.isNumber(v))
+                .validate('headers.authorization', _.isString)
             // token切分
             const tokens = chat.tokenSplit(request.headers.authorization);
-            // 随机挑选一个token
+            //随机挑选一个token
             const token = _.sample(tokens);
             const { model, messages, stream, tempature } = request.body;;
             if (stream) {
@@ -38,7 +38,7 @@ export default {
                         object: "chat.completion.chunk",
                         choices: [
                           {
-                            index: 0,
+                            index:0,
                             delta: { role: "assistant", content: err.message },
                             finish_reason: "stop",
                           },
